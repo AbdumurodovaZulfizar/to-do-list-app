07df2735205697e77135312b6ebfccc80958379e
AbdumurodovaZulfizar <abdumurodovazulfizar77@gmail.com>
2021-08-26 09:27:55 +0500

    Add tests for complete todo + correct typo errors

--- src/tests/todo_model.test.js
+++ src/tests/todo_model.test.js
@@ -21,27 +21,36 @@ test('should not be equal to Undefined', () => {
 test('should set the priority with the given value', () => {
   expect(todo.priority).toEqual('high')
 })
-test('should set the priority with the given value', () => {
+test('should not set the priority as Undefined', () => {
   expect(todo.priority).not.toBeUndefined()
 })
-test('should set the priority with the given value', () => {
+test('should not set the priority as Null', () => {
   expect(todo.priority).not.toBeNull()
 })
-test('should set the priority with the given value', () => {
+test('should set the date with the given value', () => {
   expect(todo.date).toEqual('10/10/2021')
 })
-test('should set the priority with the given value', () => {
+test('should not set the date as Undefined', () => {
   expect(todo.date).not.toBeUndefined()
 })
-test('should set the priority with the given value', () => {
+test('should not set the date as Null', () => {
   expect(todo.date).not.toBeNull()
 })
-test('should set the priority with the given value', () => {
+test('should set the description with the given value', () => {
   expect(todo.description).toEqual('Todo Description')
 })
-test('should set the priority with the given value', () => {
+test('should not set the description as Undefined', () => {
   expect(todo.description).not.toBeUndefined()
 })
-test('should set the priority with the given value', () => {
+test('should not set the description as Null', () => {
   expect(todo.description).not.toBeNull()
+})
+test('should set the isCompleted as false by default', () => {
+  expect(todo.isCompleted).toEqual(false)
+})
+test('should not set the isCompleted as Undefined', () => {
+  expect(todo.isCompleted).not.toBeUndefined()
+})
+test('should not set the isCompleted as Null', () => {
+  expect(todo.isCompleted).not.toBeNull()
 })
